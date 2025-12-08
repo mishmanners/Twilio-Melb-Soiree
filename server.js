a449fb12c68a3593663f240b428562e3876651fe
Michelle "MishManners®™" Duke <36594527+mishmanners@users.noreply.github.com>
2025-12-08 11:22:14 +1100

    remove clown

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.post('/message', async (req, res) => {
         }
 
     } else {
-        twiml.message('Please send any photo to start, and we\'ll create a fun caricature-style drawing for you 🎨🤡.');
+        twiml.message('Please send any photo to start, and we\'ll create a fun caricature-style drawing for you 🎨.');
         res.send(twiml.toString());
     }
 
