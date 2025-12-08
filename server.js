3fe78e2627384c8a3b2a947ca5463cdf7d9ff657
Michelle "MishManners®™" Duke <36594527+mishmanners@users.noreply.github.com>
2025-12-08 13:16:02 +1100

    UI testing

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.post('/message', async (req, res) => {
         }
 
     } else {
-        twiml.message('Please send any photo to start, and we\'ll create a fun caricature-style drawing for you 🎨.');
+        twiml.message('Please send any photo, and we\'ll create a fun caricature-style drawing for you 🎨.');
         res.send(twiml.toString());
     }
 
